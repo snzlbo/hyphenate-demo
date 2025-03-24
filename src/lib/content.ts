f380def9cbfe786efcf7b2ff40536b83cb66524f
snzlbo <sainzolboo619@gmail.com>
2025-03-24 15:22:28 +0800

    fix: tailwind-typography

--- src/lib/content.ts
+++ src/lib/content.ts
@@ -4,82 +4,7 @@ export const defaultEditorContent = {
     {
       type: 'heading',
       attrs: { level: 2 },
-      content: [{ type: 'text', text: 'Introducing Novel' }],
-    },
-    {
-      type: 'paragraph',
-      content: [
-        {
-          type: 'text',
-          marks: [
-            {
-              type: 'link',
-              attrs: {
-                href: 'https://github.com/steven-tey/novel',
-                target: '_blank',
-              },
-            },
-          ],
-          text: 'Novel',
-        },
-        {
-          type: 'text',
-          text: ' is a Notion-style WYSIWYG editor with AI-powered autocompletion. Built with ',
-        },
-        {
-          type: 'text',
-          marks: [
-            {
-              type: 'link',
-              attrs: {
-                href: 'https://tiptap.dev/',
-                target: '_blank',
-              },
-            },
-          ],
-          text: 'Tiptap',
-        },
-        { type: 'text', text: ' + ' },
-        {
-          type: 'text',
-          marks: [
-            {
-              type: 'link',
-              attrs: {
-                href: 'https://sdk.vercel.ai/docs',
-                target: '_blank',
-              },
-            },
-          ],
-          text: 'Vercel AI SDK',
-        },
-        { type: 'text', text: '.' },
-      ],
-    },
-    {
-      type: 'heading',
-      attrs: { level: 3 },
-      content: [{ type: 'text', text: 'Installation' }],
-    },
-    {
-      type: 'codeBlock',
-      attrs: { language: null },
-      content: [{ type: 'text', text: 'npm i novel' }],
-    },
-    {
-      type: 'heading',
-      attrs: { level: 3 },
-      content: [{ type: 'text', text: 'Usage' }],
-    },
-    {
-      type: 'codeBlock',
-      attrs: { language: null },
-      content: [
-        {
-          type: 'text',
-          text: 'import { Editor } from "novel";\n\nexport default function App() {\n  return (\n     <Editor />\n  )\n}',
-        },
-      ],
+      content: [{ type: 'text', text: 'Introducing Hyphenate Words' }],
     },
     {
       type: 'heading',
@@ -259,7 +184,7 @@ export const defaultEditorContent = {
     {
       type: 'image',
       attrs: {
-        src: 'https://public.blob.vercel-storage.com/pJrjXbdONOnAeZAZ/banner-2wQk82qTwyVgvlhTW21GIkWgqPGD2C.png',
+        src: '/banner.png',
         alt: 'banner.png',
         title: 'banner.png',
         width: null,
